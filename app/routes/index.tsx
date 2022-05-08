e4a0b711ffa2f2107283d752fcd5df46045c95df
Federico Minaya <elfedeminaya@gmail.com>
2022-05-08 02:30:02 -0300

    repo link

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -171,6 +171,17 @@ export default function Index() {
       </div>
 
       <ProductList />
+
+      <footer className="flex items-center justify-center py-8 mt-10 space-x-2 text-lg text-neutral-600">
+        <img src="/icons/github.svg" className="w-7 h-7" alt="Github" />
+        <a
+          href="https://github.com/Fedeya/aerolab-challenge"
+          target="__blank"
+          rel="noopener noreferrer"
+        >
+          View this repository
+        </a>
+      </footer>
     </div>
   );
 }
